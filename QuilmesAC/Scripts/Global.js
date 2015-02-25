1cbc44bdff676a6b773a684d50bb3abf9065b71b
John Parker <parkerjn2@vcu.edu>
2015-02-25 16:36:38 -0500

    Attempting .less with jquery

--- QuilmesAC/Scripts/Global.js
+++ QuilmesAC/Scripts/Global.js
@@ -1,11 +1,29 @@
 ﻿$(function () {
-    $("#Theme").change(function () {
-        less.modifyVars({ '@mainTheme': $(this).val() });
-        localStorage.clear();
-        less.refresh(true);
-    });
+    var hexDigits = new Array
+        ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+
+    //Function to convert hex format to a rgb color
+    function rgb2Hex(rgb) {
+        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+    }
+
+    function hex(x) {
+        return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+    }
+
+    function modifyTheme(color) {
+        less.modifyVars({ '@mainTheme': color });
+    }
 
     $("#deletePlayer").click(function () {
         return confirm('Delete all data for this player?');
+    });
+
+    $(".theme").click(function () {
+        var color = $(this).css('backgroundColor');
+        less.modifyVars({ '@blah': '100px' }, true);
+
+       // modifyTheme(rgb2Hex(color));
     });
 })
