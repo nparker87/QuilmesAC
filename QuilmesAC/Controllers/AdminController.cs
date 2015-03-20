4a0cf8f5351e6593854f562e1458757f887e8664
John Parker <parkerjn2@vcu.edu>
2015-03-20 16:22:53 -0400

    New layout

---
+++ QuilmesAC/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿namespace QuilmesAC.Controllers
+{
+    using System.Web.Mvc;
+    using ViewModels;
+
+    public class AdminController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var viewModel = new BaseViewModel
+            {
+                CurrentTab = "Admin"
+            };
+            return View(viewModel);
+        }
+    }
+}
