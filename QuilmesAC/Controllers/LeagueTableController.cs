46b18b7b828df6d124b3d14853efe3d75cfefa1c
nparker87 <wshcaps1987@gmail.com>
2015-10-05 21:40:58 -0400

    Starting a league table

---
+++ QuilmesAC/Controllers/LeagueTableController.cs
@@ -0,0 +1,12 @@
+﻿namespace QuilmesAC.Controllers
+{
+    using System.Web.Mvc;
+
+    public class LeagueTableController : Controller
+    {
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuilmesAC/ViewModels/LeagueTableViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace QuilmesAC.ViewModels
+{
+    using System.ComponentModel;
+
+    public class LeagueTableViewModel : BaseViewModel
+    {
+        public LeagueTableViewModel()
+        {
+            CurrentTab = "LeagueTable";
+        }
+
+        [DisplayName("ID")]
+        public int ID { get; set; }
+
+        [DisplayName("SeasonID")]
+        public int SeasonID { get; set; }
+
+        [DisplayName("OpponentID")]
+        public int OpponentID { get; set; }
+
+        [DisplayName("Games Played")]
+        public int GamesPlayed { get; set; }
+
+        [DisplayName("Goals For")]
+        public int GoalsFor { get; set; }
+
+        [DisplayName("Goals Against")]
+        public int GoalsAgainst { get; set; }
+    }
+}
