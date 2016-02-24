a80ce09b6b560f31be3ba3dbb339f6a72f706705
nparker87 <wshcaps1987@gmail.com>
2016-02-23 20:48:09 -0500

    About

---
+++ QuilmesAC/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿namespace QuilmesAC.Controllers
+{
+    using QuilmesAC.ViewModels;
+    using System.Web.Mvc;
+
+    public class AboutController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View(new BaseViewModel() { CurrentTab = "About" });
+        }
+    }
+}
