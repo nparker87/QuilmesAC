f7dfb9898ce9dbcd2b641d5e38db65d632d1c49a
nparker87 <wshcaps1987@gmail.com>
2016-03-29 22:39:47 -0400

    Privacy content

--- QuilmesAC/Controllers/PrivacyController.cs
+++ QuilmesAC/Controllers/PrivacyController.cs
@@ -3,7 +3,7 @@
     using System.Web.Mvc;
     using ViewModels;
 
-    public class PrivacyPolicyController : BaseController
+    public class PrivacyController : BaseController
     {
         public ActionResult Index()
         {
