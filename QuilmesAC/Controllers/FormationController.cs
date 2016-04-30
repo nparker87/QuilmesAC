aad2cf40e779201343990924a86b520c19859430
nparker87 <wshcaps1987@gmail.com>
2016-04-30 12:43:25 -0400

    Formation controller/view

---
+++ QuilmesAC/Controllers/FormationController.cs
@@ -0,0 +1,12 @@
+﻿namespace QuilmesAC.Controllers
+{
+    using System.Web.Mvc;
+
+    public class FormationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
