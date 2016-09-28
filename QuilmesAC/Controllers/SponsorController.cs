6922995bc286e4ce9e65bc98b9932a4c43899d17
Nate Laptop <parkerjn3@gmail.com>
2016-09-27 20:00:28 -0400

    Sponsor tab

---
+++ QuilmesAC/Controllers/SponsorController.cs
@@ -0,0 +1,13 @@
+﻿namespace QuilmesAC.Controllers
+{
+	using System.Web.Mvc;
+	using ViewModels;
+
+	public class SponsorController : BaseController
+	{
+		public ActionResult Index()
+		{
+			return View(new BaseViewModel() { CurrentTab = "Sponsors" });
+		}
+	}
+}
